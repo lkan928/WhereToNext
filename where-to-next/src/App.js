339bec67626f22a8fedfdaf57fad7e64351a7172
Matt-Littlefield <matt.littlefield@temple.edu>
2023-09-24 08:48:50 -0400

    added design

--- where-to-next/src/App.js
+++ where-to-next/src/App.js
@@ -1,10 +1,50 @@
-import React, { useState, useEffect } from 'react';
+import React, { useRef,useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import scrapeWebsite from './scraper.js';
+import Colors from './colors.js'
+
 
 function App() {
   const [pageData, setPageData] = useState({ pageTitle: "", headingText: "" });
+  
+  const canvasRef = useRef();
+
+  const drawRectangle = () => {
+    const context = canvasRef.current.getContext("2d");
+    context.strokeStyle = Colors.EGGSHELL;
+    context.lineWidth = 2;
+    context.strokeRect(50, 20, 195, 75);
+    
+  };
+  const drawBackground = () => {
+    const context = canvasRef.current.getContext("2d");
+    context.strokeStyle = Colors.AUBURN;
+    context.lineWidth = 2;
+    context.fillStyle = Colors.AUBURN;
+    context.fillRect(0, 0, 300, 250);
+  };
+  const drawFRectangle = () => {
+    const context = canvasRef.current.getContext("2d");
+    context.strokeStyle = Colors.AMARANTH;
+    context.lineWidth = 2;
+    context.fillStyle = Colors.VIOLET;
+    context.fillRect(120, 100, 50, 25);
+  };
+  const drawText = () => {
+    const context = canvasRef.current.getContext("2d");
+    context.font = "8px arial";
+    context.strokeStyle = Colors.FLORAL;
+    context.strokeText("Go!", 138, 115);
+  };
+
+  useEffect(() => {
+    drawBackground();
+    drawRectangle();
+    drawFRectangle();
+    drawText();
+  }, []);
+  
 
   useEffect(() => {
     scrapeWebsite("https://www.temple.edu/")
@@ -18,25 +58,43 @@ function App() {
 
   return (
     <div className="App">
+      <canvas
+    ref={canvasRef}
+    style={{
+      width: "1500px",
+      height: "800px",
+      background: "url('./bg-img.jpg')",
+    }}
+  />
+    <script>
+    var canvas = document.getElementById('canvas');
+        var ctx = canvas.getContext('2d');
+              //window.devicePixelRatio=1; //Blury Text
+        window.devicePixelRatio=2;      //Clear Text
+        //(CSS pixels).
+              //Display Size
+        var size = 150;
+        canvas.style.width = size + "px";
+        canvas.style.height = size + "px";
+  
+        var scale = window.devicePixelRatio; 
+            
+        canvas.width = Math.floor(size * scale);
+        canvas.height = Math.floor(size * scale);
+    </script>
+      
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <a class="btn btn-large btn-info" href="https://www.howtocanvas.com/">Home</a>
         <p>
           Page Title: {pageData.pageTitle}
         </p>
         <p>
           Heading Text: {pageData.headingText}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
 }
+
 
 export default App;
---
+++ where-to-next/src/colors.js
@@ -0,0 +1,8 @@
+const Colors = {
+AUBURN: '#A62639',
+AMARANTH: '#DB324D',
+VIOLET: '#5C5D8D',
+FLORAL: '#FBFBF2',
+EGGSHELL: '#F4EBD9',
+}
+export default Colors;
